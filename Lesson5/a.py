8c9108d1462699dbf19bf84e21fbb4cad407c2b5
endlesslust444 <alannxu1472580@gmail.com>
2025-04-29 18:46:36 +0800

    Add files via upload

---
+++ Lesson5/a.py
@@ -0,0 +1,19 @@
+print('fuck \nanjelica')
+#use \n to move to the next line
+
+print('''juneliu
+      hkdoll
+      wmm''')
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+# After assigning 'XYZ' to 'a', 'b' remains 'ABC' because it was assigned the value of 'a' before the change.
+
+c = 10 // 3
+print(c)
+# // means "integer division" or "floor division", which only keeps the integer part of the result.
+
+d=8%3
+print(d)
+# %means only keeps the remiander
